8dcf493df2585c2de65571da711cf48e90731055
hema325 <hema24790@gmail.com>
2024-10-04 12:50:35 +0300

    vvv

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+//vvvvvv
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

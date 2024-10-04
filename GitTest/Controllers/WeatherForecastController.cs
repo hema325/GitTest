bdd6c37cf87028ae0b83639f31a513f840617516
hema325 <hema24790@gmail.com>
2024-10-04 14:25:06 +0300

    nn

--- GitTest/Controllers/WeatherForecastController.cs
+++ GitTest/Controllers/WeatherForecastController.cs
@@ -23,6 +23,7 @@ namespace GitTest.Controllers
         {//ddddcc
             // edit 2 Test branch
             // ttttttt
+            // vvvvvvvv
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
